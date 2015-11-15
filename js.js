d45ee6cb12e6733c5ed76551c1f8c12c258d93b8
eliezerwohl <eliezerwohl@gmail.com>
2015-11-15 10:51:14 -0500

    button now adds row

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-debugger;
+
 // get all of the delete buttons
 var deleteButtons = document.getElementsByClassName("delete-btn");
 
@@ -13,3 +13,30 @@ for(i = 0; i < deleteButtons.length; i++){
 function deleteRecord(){
   this.parentElement.parentElement.remove();
 }
+
+// add form field with + 
+var addRow = document.getElementsByClassName("add-row")
+
+
+// //add event istener
+
+for(i = 0; i < addRow.length; i++){
+  addRow[i].addEventListener("click", goRow);
+}
+
+
+
+function goRow() {
+    var table = document.getElementById("myTable");
+    var row = table.insertRow(5);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    cell1.innerHTML = "NEW CELL1";
+    cell2.innerHTML = "NEW CELL2";
+}
+
+
+// //function to add row
+
