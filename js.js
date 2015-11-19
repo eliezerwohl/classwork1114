e0582611cc1e0db2d01f8679119d2fd8a60c6215
eliezerwohl <eliezerwohl@gmail.com>
2015-11-19 16:14:58 -0500

    added new code to add row, now delete button work

--- js.js
+++ js.js
@@ -15,7 +15,6 @@ function deleteRecord(){
 }
 
 
-
 document.getElementById("goRow").addEventListener("click", check)
 
 
@@ -32,33 +31,58 @@ function check () {
         prompt("hey! enter a email!");
     }
 
-
-
-   
-
-    else { newRow()
+    else {
+        addRow()
+    }
+    // else { newRow()
+    
 }
 
+//         function newRow() {
+//     var table = document.getElementById("myTable");
+//     var row = table.insertRow(1);
+//     var cell1 = row.insertCell(0);
+//     var cell2 = row.insertCell(1);
+//     var cell3 = row.insertCell(2);
+//     var cell4 = row.insertCell(3);
+//     cell1.innerHTML = document.getElementById("firstName").value;
+//     cell2.innerHTML = document.getElementById("lastName").value;
+//     cell3.innerHTML = document.getElementById("email").value;
+//     cell4.innerHTML = "<a href="#"> bong </a>";
+// ;
+// }
+
+
+function addRow(){
+  var myTable = document.getElementById('myTable');
+  var firstName = document.getElementById('firstName');
+  var lastName = document.getElementById('lastName');
+  var email = document.getElementById('email');
+
+
+   var newRow = myTable.insertRow(myTable.rows.length - 1);
+
+  for(var i=0; i<4; i++){
+    var td = document.createElement('td');
+    var delButton = document.createElement('button');
+
+    if(i===0){
+      td.textContent = firstName.value;
+    }if(i===1){
+      td.textContent = lastName.value;
+    }if(i===2){
+      td.textContent = email.value;
+    }if(i===3){
+      delButton.className += "warning delete-btn";
+      delButton.textContent = 'X';
+      td.appendChild(delButton);
+      delButton.addEventListener('click', deleteRecord);
     }
 
+    newRow.appendChild(td);
+  }
 
-
-
-
-
-        function newRow() {
-    var table = document.getElementById("myTable");
-    var row = table.insertRow(1);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    var cell4 = row.insertCell(3);
-    cell1.innerHTML = document.getElementById("firstName").value;
-    cell2.innerHTML = document.getElementById("lastName").value;
-    cell3.innerHTML = document.getElementById("email").value;
-    cell4.innerHTML = " ";
 }
-
 
 // var myTimer = setInterval(function()
 // { 
